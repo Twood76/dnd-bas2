389c2c3344b0548252693461d445731e668c384b
Samuel <samuel.dan@web.de>
2019-03-02 20:12:27 +0100

    Added heightButton.

--- App/SettingsLayout.cs
+++ App/SettingsLayout.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using Android.App;
+using Android.Content;
 using Android.Text;
 using Android.Widget;
+
+using App.DnD.Mechanics;
 
 namespace App
 {
@@ -18,26 +22,44 @@ namespace App
         {
             this.Orientation = GridOrientation.Vertical;
 
-            TextView heightDescriptor = new TextView(context)
+            Button heightButton = new Button(context)
             {
-                Text = "The height of every button (in px): "
+                Text = "Sets the height of every button in pixels. Current: " + Convert.ToString(context.Settings.ExpectedHeight),
             };
-            EditText height = new EditText(context)
+            heightButton.SetHeight(context.Settings.ExpectedHeight);
+            heightButton.Click += (object sender, EventArgs args) =>
             {
-                InputType = Android.Text.InputTypes.ClassNumber,
-                Text = Convert.ToString(context.Settings.ExpectedHeight)
-            };
-            height.TextChanged += (object sender, TextChangedEventArgs args) =>
-            {
-                try
+                AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+                dialog.SetTitle("Button height");
+
+                GridLayout num = new GridLayout(dialog.Context);
+                EditText n = new EditText(dialog.Context)
                 {
-                    context.Settings.ExpectedHeight = Convert.ToInt32(height.Text);
-                    context.SaveSettings();
-                }
-                catch (Exception) { }
+                    InputType = InputTypes.ClassNumber,
+                    Text = Convert.ToString(context.Settings.ExpectedHeight)
+                };
+                num.AddView(n);
+                dialog.SetView(num);
+
+                dialog.SetPositiveButton("OK!", (object s, DialogClickEventArgs a) =>
+                {
+                    if (n.Text != "")
+                    {
+                        context.Settings.ExpectedHeight = Convert.ToInt32(n.Text);
+                        context.SaveSettings();
+                    }
+                });
+                dialog.Create().Show();
             };
-            this.AddView(heightDescriptor);
-            this.AddView(height);
+            this.AddView(heightButton);
+
+            Button reseedButton = new Button(context)
+            {
+                Text = "Reseeds the generator for random numbers using the current time."
+            };
+            reseedButton.SetHeight(context.Settings.ExpectedHeight);
+            reseedButton.Click += (object sender, EventArgs args) => Dice.Reseed();
+            this.AddView(reseedButton);
         }
     }
 }
